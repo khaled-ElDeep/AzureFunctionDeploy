fdc7caf87d116709e5ad8e4c3b4072086bd95ff0
khaled-ElDeep <khaledseif1010@gmail.com>
2025-11-01 11:17:56 +0200

    new function update

--- function_app.py
+++ function_app.py
@@ -17,9 +17,9 @@ def CICDfunction(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"go go, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response. gogo",
              status_code=200
         )
